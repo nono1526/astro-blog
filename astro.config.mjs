31a66a135e232900179e6350e6e13be390c04d4c
Nono <nono1102137211@gmail.com>
2022-05-07 13:14:07 +0800

    feat: tailwind use custom style

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), vue(), tailwind()]
+  integrations: [preact(), vue(),
+    tailwind({
+      config: { applyBaseStyles: false }
+    })]
 });
