ec5e42b7cc9712fc5661e94fa14029d9029892c8
Nono Chao <nonochao@kkbox.com>
2023-04-25 17:35:41 +0800

    feat: migrate to astro v2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
 import { defineConfig } from 'astro/config';
-import preact from '@astrojs/preact';
+import mdx from '@astrojs/mdx';
 
-import vue from "@astrojs/vue";
-import tailwind from "@astrojs/tailwind";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), vue(),
-    tailwind({
-      config: { applyBaseStyles: false }
-    })]
+	site: 'https://example.com',
+	integrations: [mdx(), sitemap()],
 });
